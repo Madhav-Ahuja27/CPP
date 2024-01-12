fc1a0831b627da364c829086eadbd2ecb1621ba1
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-12 20:26:09 +0530

    Create mathFunctions.cpp

---
+++ mathFunctions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Enter two numbers";
+	int a,b;
+	cin>>a>>b;
+	cout<<"The addition is:"<<a+b;
+	cout<<"The subtraction is:"<<a-b;
+	cout<<"The multiplication is:"<<a*b;
+	cout<<"The division is:"<<a/b;
+}
