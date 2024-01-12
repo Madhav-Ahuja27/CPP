537ce6d0f320010d6300da953078f0b282b5dabe
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-12 20:23:27 +0530

    Create areaPerimeterRect.cpp

---
+++ areaPerimeterRect.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int l,b;
+	cout<<"Enter the length\t";
+	cin>>l;
+	cout<<"Enter the breadth\t";
+	cin>>b;
+	cout<<"The area of the rectangle is:"<<l*b;
+	cout<<endl<<"The perimeter of the rectangle is:"<<2*(l+b);
+}
