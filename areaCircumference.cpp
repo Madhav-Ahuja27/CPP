d519a51f2a25a50cb770d1f05a440d1545a49393
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-12 20:26:56 +0530

    Create areaCircumference.cpp

---
+++ areaCircumference.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int radius;
+	cout<<"Enter the radius of the circle";
+	cin>>radius;
+	cout<<"The area of the circle is:"<<3.14*radius*radius;
+	cout<<endl<<"The circumference of the circle is:"<<2*3.14*radius;
+}
