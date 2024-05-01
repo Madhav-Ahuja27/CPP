d438ff3e899a7f152ebdaccb817c470d43d8d45a
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-05-02 00:01:09 +0530

    Create stringErase.cpp

---
+++ stringErase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string s = "abcd";
+  string t = "abcde";
+  // for(int i = 0 ; i < s.length() ; i++){
+  //   for(int j = 0 ; j < t.length() ; j++){
+  //       if(s[i] == t[j]){
+  //           s.erase(i);
+  //           t.erase(j);
+  //           cout<<"S : "<<s<<endl;
+  //           cout<<"T : "<<t<<endl;
+  //       }
+  //   }
+  // }
+  s.erase[0];
+  cout<<s;
+ 
+}
